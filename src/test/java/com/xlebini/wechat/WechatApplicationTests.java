787bdf436c8ae38f84ee6fb499b4db95774081b1
Pavel Minin <minin09092002@mail.ru>
2024-11-06 11:24:04 +0300

    init commit

---
+++ src/test/java/com/xlebini/wechat/WechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xlebini.wechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
